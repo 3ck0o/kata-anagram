160131017eb3816ed3e2d06f155dc70551e68a8d
3ck0 <gaetan.lemoing@outlook.fr>
2017-07-02 21:07:47 +0200

    Updated version

--- src/main.kt
+++ src/main.kt
@@ -6,40 +6,28 @@ val file = "wordlist.txt"
 
 fun main(args: Array<String>) {
     val elapsedTime = measureTimeMillis {
-        val reference = args[0]
-        val charCount = countChars(reference)
-        File(file).bufferedReader(Charsets.ISO_8859_1).useLines {
-            lines ->
-            lines.forEach { displayAnagram(charCount, reference.length, it) }
-        }
+        val anagrams: MutableMap<String, MutableList<String>> = HashMap()
+        File(file).bufferedReader(Charsets.ISO_8859_1).forEachLine { line -> updateAnagramList(anagrams, line.toLowerCase()) }
+        anagrams.forEach(::displayAnagram)
     }
-    println("Finished in $elapsedTime ms")
+    println("Needed $elapsedTime ms")
 }
 
-fun displayAnagram(charCount: Map<Char, Int>, referenceLength: Int, toCompare: String) {
-    if(isAnagram(charCount, referenceLength, toCompare))
-        println(toCompare)
+fun displayAnagram(anagram: Map.Entry<String, MutableList<String>>) {
+    if (anagram.value.size > 1)
+        println(anagram.value)
 }
 
-fun isAnagram(charCount: Map<Char, Int>, referenceLength: Int, toCompare: String): Boolean {
-    if (toCompare.length != referenceLength)
-        return false
-    for ((char, count) in charCount) {
-        if (toCompare.toLowerCase().count { comparedChar -> comparedChar == char } != count)
-            return false
-    }
-    return true
+fun updateAnagramList(anagrams: MutableMap<String, MutableList<String>>, line: String) {
+    val key = getKey(line)
+    if (anagrams[key] == null)
+        anagrams[key] = mutableListOf(line)
+    else
+        anagrams[key]!!.add(line)
 }
 
-fun countChars(reference: String): Map<Char, Int> {
-    val preparedWord: MutableMap<Char, Int> = HashMap()
-    reference.toLowerCase().forEach { char -> handleCharCount(preparedWord, char) }
-    return preparedWord
-}
-
-fun handleCharCount(anagramData: MutableMap<Char, Int>, char: Char) {
-    if (anagramData[char] == null) {
-        anagramData.put(char, 0)
-    }
-    anagramData.put(char, anagramData[char]!! + 1)
+fun getKey(word: String): String {
+    val charArray = word.toCharArray()
+    Arrays.sort(charArray)
+    return String(charArray)
 }
